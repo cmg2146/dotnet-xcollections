e37a69a655ad62b92d57ab634bf421700250d301
Clark Granum <120140190+cmg2146@users.noreply.github.com>
2025-03-27 10:47:16 -0700

    initial implementation of SequentialDictionary

---
+++ src/LruCache.cs
@@ -0,0 +1,113 @@
+namespace Cmg.Dotnet.XCollections;
+
+/// <summary>
+/// An implementation of a Least Recently Used cache
+/// </summary>
+/// <typeparam name="TKey"></typeparam>
+/// <typeparam name="TValue"></typeparam>
+public class LruCache<TKey, TValue> where TKey : notnull
+{
+    private long? _capacity = null;
+
+    /// <summary>
+    /// The maximum number of items that can be added to the cache. If null, the cache is unlimited.
+    /// </summary>
+    /// <exception cref="ArgumentException">If capacity is set to a value less than 1</exception>
+    public long? Capacity
+    {
+        get
+        {
+            return _capacity;
+        }
+        set
+        {
+            if (value.HasValue && (value < 1))
+            {
+                throw new ArgumentException("Capacity must be greater than 0");
+            }
+            _capacity = value;
+        }
+    }
+
+    private readonly SequentialDictionary<TKey, TValue> _cache;
+    private readonly object _lock = new();
+
+    /// <summary>
+    /// Constructs an instance of an LruCache
+    /// </summary>
+    /// <param name="capacity">The maximum number of items that can be added to the cache. If null, the cache is unlimited.</param>
+    public LruCache(long? capacity)
+    {
+        _cache = new SequentialDictionary<TKey, TValue>();
+
+        Capacity = capacity;
+    }
+
+    /// <summary>
+    /// Gets the number of items currently in the cache.
+    /// </summary>
+    public int Count => _cache.Count;
+
+    /// <summary>
+    /// Gets the item with specified key.
+    /// </summary>
+    /// <param name="key"></param>
+    /// <returns>
+    /// The item with the specified key or null if key is not found. If the key is found, the
+    /// associated item is moved to the end of the cache.
+    /// </returns>
+    public TValue? Get(TKey key)
+    {
+        lock (_lock)
+        {
+            if (_cache.TryGetValue(key, out var value))
+            {
+                // any time an item is retrieved, move it to the end of the cache
+                _cache.MoveToLast(key);
+            }
+
+            return value;
+        }
+    }
+
+    /// <summary>
+    /// Adds or Updates an item in the cache
+    /// </summary>
+    /// <param name="key"></param>
+    /// <param name="value"></param>
+    public void Put(TKey key, TValue value)
+    {
+        lock (_lock)
+        {
+            // call Get so if item exists it is moved to end of cache
+            var item = Get(key);
+
+            // if item wasnt found then we will be adding a new entry
+            // WARNING: What if null is a valid value?
+            if (item == null)
+            {
+                // if there isnt room left, delete oldest entry from cache
+                if (Count == Capacity)
+                {
+                    _cache.RemoveFirst();
+                }
+            }
+
+            // add or update cached value
+            _cache[key] = value;
+        }
+    }
+
+    /// <summary>
+    /// Deletes an item from the cache.
+    /// </summary>
+    /// <param name="key"></param>
+    /// <returns></returns>
+    public bool Delete(TKey key)
+    {
+        lock (_lock)
+        {
+            return _cache.Remove(key);
+        }
+    }
+}
---
+++ src/SequentialDictionary.cs
@@ -0,0 +1,205 @@
+namespace Cmg.Dotnet.XCollections;
+
+using System.Diagnostics.CodeAnalysis;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// A dictionary that uses a doubly linked list for underlying storage such that the most recently
+/// added item occupies the last element in the linked list and the oldest item occupies the first
+/// element in the list.
+/// </summary>
+/// <typeparam name="TKey"></typeparam>
+/// <typeparam name="TValue"></typeparam>
+/// <remarks>
+/// This class is not thread safe.
+/// </remarks>
+public class SequentialDictionary<TKey, TValue> : IDictionary<TKey, TValue> where TKey : notnull
+{
+    private readonly IDictionary<TKey, LinkedListNode<KeyValuePair<TKey, TValue>>> _dict;
+    private readonly LinkedList<KeyValuePair<TKey, TValue>> _storage;
+
+    /// <summary>
+    /// Constructs an instance of a SequentialDictionary
+    /// </summary>
+    public SequentialDictionary()
+    {
+        _dict = new Dictionary<TKey, LinkedListNode<KeyValuePair<TKey, TValue>>>();
+        _storage = new LinkedList<KeyValuePair<TKey, TValue>>();
+    }
+
+    /// <inheritdoc/>
+    public bool IsReadOnly { get; }
+    /// <inheritdoc/>
+    public int Count => _dict.Count;
+
+    /// <inheritdoc/>
+    public TValue this[TKey key]
+    {
+        get
+        {
+            // dont catch exceptions and let them bubble up if key is not found
+            return _dict[key].Value.Value;
+        }
+        set
+        {
+            var item = new KeyValuePair<TKey, TValue>(key, value);
+
+            if (ContainsKey(key))
+            {
+                _dict[key].Value = item;
+            }
+            else
+            {
+                Add(item);
+            }
+        }
+    }
+
+    /// <inheritdoc/>
+    public ICollection<TKey> Keys => _dict.Keys;
+    /// <inheritdoc/>
+    public ICollection<TValue> Values => _storage.Select(x => x.Value).ToList();
+
+    /// <inheritdoc/>
+    public void Clear()
+    {
+        _dict.Clear();
+        _storage.Clear();
+    }
+
+    /// <inheritdoc/>
+    public void Add(TKey key, TValue value)
+    {
+        Add(new KeyValuePair<TKey, TValue>(key, value));
+    }
+
+    /// <inheritdoc/>
+    public void Add(KeyValuePair<TKey, TValue> item)
+    {
+        if (ContainsKey(item.Key))
+        {
+            throw new ArgumentException($"key {item.Key} already exists");
+        }
+
+        _dict[item.Key] = _storage.AddLast(item);
+    }
+
+    /// <inheritdoc/>
+    public bool Remove(TKey key)
+    {
+        return ContainsKey(key) && RemoveNode(_dict[key]);
+    }
+
+    /// <inheritdoc/>
+    public bool Remove(KeyValuePair<TKey, TValue> item)
+    {
+        // need to check if the item with the specified key and value exists first
+        return Contains(item) && Remove(item.Key);
+    }
+
+    /// <summary>
+    /// Removes the first element from the dictionary.
+    /// </summary>
+    ///
+    /// <remarks>
+    /// The first element in the dictionary is always stored as the first element in the underlying
+    /// linked list.
+    /// </remarks>
+    public bool RemoveFirst()
+    {
+        return (_storage.First != null) && RemoveNode(_storage.First);
+    }
+
+    /// <summary>
+    /// Removes the last element from the dictionary.
+    /// </summary>
+    ///
+    /// <remarks>
+    /// The last element in the dictionary is always stored as the last element in the underlying
+    /// linked list.
+    /// </remarks>
+    public bool RemoveLast()
+    {
+        return (_storage.Last != null) && RemoveNode(_storage.Last);
+    }
+
+    private bool RemoveNode(LinkedListNode<KeyValuePair<TKey, TValue>> node)
+    {
+        var removed = _dict.Remove(node.Value.Key);
+        if (!removed)
+        {
+            return false;
+        }
+
+        _storage.Remove(node);
+
+        return true;
+    }
+
+    /// <inheritdoc/>
+    public bool ContainsKey(TKey key)
+    {
+        return _dict.ContainsKey(key);
+    }
+
+    /// <inheritdoc/>
+    public bool TryGetValue(TKey key, [MaybeNullWhen(false)] out TValue value)
+    {
+        if (!_dict.TryGetValue(key, out var node))
+        {
+            value = default;
+            return false;
+        }
+
+        value = node.Value.Value;
+        return true;
+    }
+
+    /// <inheritdoc/>
+    public bool Contains(KeyValuePair<TKey, TValue> item)
+    {
+        return _storage.Contains(item);
+    }
+
+    /// <inheritdoc/>
+    public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
+    {
+        _storage.CopyTo(array, arrayIndex);
+    }
+
+    /// <inheritdoc/>
+    public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+    {
+        return _storage.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    /// <summary>
+    /// Move the specified entry to the start of the dictionary.
+    /// </summary>
+    /// <exception cref="KeyNotFoundException">key is not found</exception>
+    public void MoveToFirst(TKey key)
+    {
+        // will raise an exception if key is not found
+        var node = _dict[key];
+        _storage.Remove(node);
+        _storage.AddFirst(node);
+    }
+
+    /// <summary>
+    /// Move the specified entry to the end of the dictionary.
+    /// </summary>
+    /// <exception cref="KeyNotFoundException">key is not found</exception>
+    public void MoveToLast(TKey key)
+    {
+        // will raise an exception if key is not found
+        var node = _dict[key];
+        _storage.Remove(node);
+        _storage.AddLast(node);
+    }
+}
